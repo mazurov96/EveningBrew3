171459d6f816568732b38f1448bffb556c353069
User <User@Asus>
2017-05-09 20:21:23 +0300

    Косметика

--- src/Maps/StarTownFrame.java
+++ src/Maps/StarTownFrame.java
@@ -35,6 +35,7 @@ public class StarTownFrame extends javax.swing.JFrame {
 
         jPanel2 = new javax.swing.JPanel();
         jButton2 = new javax.swing.JButton();
+        jLabel5 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
@@ -57,9 +58,15 @@ public class StarTownFrame extends javax.swing.JFrame {
         jPanel2.add(jButton2);
         jButton2.setBounds(923, 603, 130, 50);
 
-        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/MapsFoto/Старый город1_1.jpg"))); // NOI18N
+        jLabel5.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(255, 0, 0));
+        jLabel5.setText("Cтарый город");
+        jPanel2.add(jLabel5);
+        jLabel5.setBounds(40, 30, 320, 80);
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/MapsFoto/stgotod1.jpg"))); // NOI18N
         jPanel2.add(jLabel2);
-        jLabel2.setBounds(10, 10, 750, 650);
+        jLabel2.setBounds(20, 100, 620, 570);
 
         jLabel4.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
         jLabel4.setForeground(new java.awt.Color(255, 255, 51));
@@ -137,6 +144,7 @@ public class StarTownFrame extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel2;
     // End of variables declaration//GEN-END:variables
 }
